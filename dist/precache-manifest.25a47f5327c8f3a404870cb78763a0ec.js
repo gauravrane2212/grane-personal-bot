23f6fd83f0c5f541011913a6a1cdbc77c938e16c
Gaurav Rane <gaurav.v.rane@gmail.com>
2018-10-23 22:05:56 -0400

    Revert "Merge pull request #2 from gauravrane2212/removeDist"
    This reverts commit 43a2418ba27944f4e68b58d17b1c3bffbd9fb2e4, reversing
    changes made to e7f06814e09fdb01c2cb12e7e2a047c9d224d4e6.

---
+++ dist/precache-manifest.25a47f5327c8f3a404870cb78763a0ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "a5cf0671a12be025c4f4",
+    "url": "/js/chunk-vendors.44cf1e0e.js"
+  },
+  {
+    "revision": "161842194f207370818b",
+    "url": "/js/app.dd3ba56d.js"
+  },
+  {
+    "revision": "da1eaa38473577995c90",
+    "url": "/js/about.8a391d75.js"
+  },
+  {
+    "revision": "1ca5f1128dd9c845a1dba1fb2997b234",
+    "url": "/index.html"
+  },
+  {
+    "revision": "161842194f207370818b",
+    "url": "/css/app.512b5cc0.css"
+  }
+];
