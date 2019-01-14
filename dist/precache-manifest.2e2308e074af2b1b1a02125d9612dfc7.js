98912916bd04a70909fcc38f773494adfcde65b8
Gaurav Rane <gaurav.v.rane@gmail.com>
2019-01-13 19:36:49 -0500

    feat(postgres): adds local database support

---
+++ dist/precache-manifest.2e2308e074af2b1b1a02125d9612dfc7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "67190c1382d078ddec32",
+    "url": "/js/chunk-vendors.318f9d51.js"
+  },
+  {
+    "revision": "7a3658422ad9d81a970e",
+    "url": "/js/app.0b26af56.js"
+  },
+  {
+    "revision": "7ec7502e494bf496574b",
+    "url": "/js/about.5273d30c.js"
+  },
+  {
+    "revision": "b8877b37a9ba780678f1c7d0a067716f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a3658422ad9d81a970e",
+    "url": "/css/app.ac906db3.css"
+  }
+];
